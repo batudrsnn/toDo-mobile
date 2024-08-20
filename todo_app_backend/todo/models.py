e46c9fb5526ec0fcc3d9fb35a2c8fa2428ca0e53
Batuhan <batu@Batus-MacBook-Pro.local>
2024-08-20 17:52:42 +0200

    Normal User and UserAdmin models are defined in model.py and admin.py

--- todo_app_backend/todo/models.py
+++ todo_app_backend/todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length = 50, blank = False, unique = True)
+    last_name = models.CharField(max_length = 20)
+    
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    
+    def __str__(self):
+      return "{}".format(self.username)
+    
+    
--- todo_app_backend/todo_app_backend/settings.py
+++ todo_app_backend/todo_app_backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -80,7 +83,7 @@ DATABASES = {
     }
 }
 
-
+AUTH_USER_MODEL = 'todo.User'
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
