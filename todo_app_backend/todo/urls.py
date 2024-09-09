ab544b52b1151dfae8b52bb5a239d5e544294424
Batu <93510339+batudrsnn@users.noreply.github.com>
2024-09-09 12:49:19 +0200

    Delete todo_app_backend directory

--- todo_app_backend/todo/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('todo/', views.ToDoListView.as_view()),
-    path('todo/<int:user_id>', views.ToDoListView.as_view()),
-    path('todo-detail/<int:user_id>/<int:todo_id>/', views.ToDoListDetailView.as_view()),
-]
