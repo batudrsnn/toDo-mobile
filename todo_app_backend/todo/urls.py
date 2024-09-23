f074db3b17694a1cbfe698af23902841ae572a99
Batuhan <batu@Batus-MacBook-Pro.local>
2024-09-23 20:55:56 +0200

    token auth setup

--- todo_app_backend/todo/urls.py
+++ todo_app_backend/todo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *  # Import your viewset
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 # Create a router and register the viewset
 router = DefaultRouter()
@@ -10,4 +12,9 @@ router.register(r'appUsers', appUserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),  # Include the router-generated URLs
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+
+""" TOKEN YOLLA LOGIN DE """
