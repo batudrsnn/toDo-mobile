cc20adb9639ec27fabfb5db0245810339d772bd9
Batuhan <batu@Batus-MacBook-Pro.local>
2024-09-26 18:04:42 +0200

    fix

--- todo_app_backend/todo_app/settings.py
+++ todo_app_backend/todo_app/settings.py
@@ -138,8 +138,9 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAdminUser'
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        #'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
     ),
 }
 
