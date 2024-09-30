53899f81d4daab3980f5460a1c29eaaa7590ce96
Batuhan <batu@Batus-MacBook-Pro.local>
2024-09-30 17:47:51 +0200

    pymysql added into settings

--- todo_app_backend/todo_app/settings.py
+++ todo_app_backend/todo_app/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
