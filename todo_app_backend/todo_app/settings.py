e790af122d3ddd7eb94da30e72144fc15543afed
Batuhan <batu@Batus-MacBook-Pro.local>
2024-10-04 11:59:46 +0200

    token time 1h

--- todo_app_backend/todo_app/settings.py
+++ todo_app_backend/todo_app/settings.py
@@ -152,7 +152,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
  #   'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
  #   'SLIDING_TOKEN_LIFETIME': timedelta(days=30),
  #   'SLIDING_TOKEN_REFRESH_LIFETIME_LATE_USER': timedelta(days=1),
